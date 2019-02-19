e0f047c5acaaefe1ab9d87456191a988cdeb4c51
Ian King <king.ian.ilk@gmail.com>
2019-02-19 16:22:34 -0500

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <NavMenu />
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavMenu from "@/components/NavMenu.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    NavMenu,
+    HelloWorld
+  }
+};
+</script>
