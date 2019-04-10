6c476124d06d1086720478d157142ed949e10810
Ian King <king.ian.ilk@gmail.com>
2019-04-10 09:02:49 -0400

    Huge commit incoming

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <NavigationBar />
+    <div class="content-area">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
+
+<script>
+import NavigationBar from "@/components/NavigationBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavigationBar
+  }
+};
+</script>
 
 <style lang="less">
 #app {
@@ -15,22 +25,13 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: white;
   padding: 15px;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-.vsm-collapsed {
-  .vsm-dropdown {
-    width: fit-content;
+  background: url(/body-bg.jpg);
+
+  .content-area {
+    background-color: white;
+    padding: 10px 15px;
+    padding-top: 30px;
   }
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import Vue from "vue";
-import Router from "vue-router";
+import VueRouter from "vue-router";
 import Home from "./views/Home.vue";
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
+export default new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
@@ -19,8 +19,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
 });
